30532d3f2b7ead82ec0d92d22ba565fd9d29a88a
01fe23bee109 <147721248+01fe23bee109@users.noreply.github.com>
2026-03-31 15:37:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import google.generativeai as genai
 api_key = st.secrets["API_KEY"]
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 st.title("AI Study Assistant")
 
 q = st.text_input("Ask a question:")
 
 if q:
-    response = model.generate_content([q])
+    response = model.generate_content(q)
     st.write(response.text)
