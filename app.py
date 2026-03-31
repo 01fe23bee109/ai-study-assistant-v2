cf12f93c2f4a638e4e281a58a354fcd0b9bde991
01fe23bee109 <147721248+01fe23bee109@users.noreply.github.com>
2026-03-31 15:43:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ st.title("AI Study Assistant")
 q = st.text_input("Ask a question:")
 
 if q:
-    response = model.generate_content(f"Explain clearly: {q}")
-    st.write(response.text)
+    response = model.generate_content(q)
+    st.write(response.candidates[0].content.parts[0].text)
